94f569d3c3e00f6014be02c959c7601e5e3d9416
tatata1221 <theanh0899@gmail.com>
2021-05-21 10:32:34 +0700

    save todo on firebase

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,59 +1,30 @@
-import React from "react";
+import Input from './Input';
+import Filter from './Filter';
 
-/* 
-  【Todoのデータ構成】
-　・key：Todoを特定するID（String）
-　・text：Todoの内容（String）
-　・done：完了状態（Boolean true:完了済み,, false:未完了）
-*/
+import useFirestore from '../hooks/useFirestore';
 
-/* コンポーネント */
-import TodoItem from "./TodoItem";
-import Input from "./Input";
-import Filter from "./Filter";
+ function Todo() {
+    const [items, addItem, updateItem, clearItems] = useFirestore();
 
-/* カスタムフック */
-import useStorage from "../hooks/storage";
+   const [filter, setFilter] = React.useState('ALL');
 
-/* ライブラリ */
-import { getKey } from "../lib/util";
+	@@ -19,17 +19,11 @@ import useStorage from '../hooks/useStorage';
+   });
 
-function Todo() {
-  const [items, putItems, clearItems] = useStorage();
+   const handleCheck = checked => {
+    updateItem(checked);
+ };
 
-  const [filter, setFilter] = React.useState("ALL");
-
-  const handleAdd = (text) => {
-    putItems([...items, { key: getKey(), text, done: false }]);
+   const handleAdd = text => {
+     addItem({ text, done: false });
   };
 
-  const displayItems = items.filter((item) => {
-    if (filter === "ALL") return true;
-    if (filter === "TODO") return !item.done;
-    if (filter === "DONE") return item.done;
-    return null;
-  });
-
-  const handleFilterChange = (value) => setFilter(value);
-
-  return (
-    <div className="panel">
-      <div className="panel-heading">ITSS ToDoアプリ</div>
-      <Input onAdd={handleAdd} />
-
-      <Filter onChange={handleFilterChange} value={filter} />
-
-      {displayItems.map((item) => (
-        <TodoItem key={item.key} item={item} />
-      ))}
-      <div className="panel-block">{displayItems.length} items</div>
-      <div className="panel-block">
-        <button className="button is-light is-fullwidth" onClick={clearItems}>
-          全てのToDoを削除
-        </button>
-      </div>
-    </div>
-  );
-}
-
-export default Todo;
+  const handleFilterChange = value => setFilter(value);
+	@@ -50,7 +44,7 @@ import useStorage from '../hooks/useStorage';
+       />
+       {displayItems.map(item => (
+         <TodoItem 
+           key={item.id}
+           item={item}
+           onCheck={handleCheck}
+         />
