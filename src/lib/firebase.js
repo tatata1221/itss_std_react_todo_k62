9728fd202528851dcd4dbf8d662b2a8f58471484
tatata1221 <theanh0899@gmail.com>
2021-05-21 10:27:49 +0700

    use firebase

---
+++ src/lib/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase'
+
+var firebaseConfig = {
+    apiKey: "AIzaSyB9kPk3HIkyW7J99D4CdLtkwQkc74R2jqQ",
+    authDomain: "fir-sample-e4e31.firebaseapp.com",
+    projectId: "fir-sample-e4e31",
+    storageBucket: "fir-sample-e4e31.appspot.com",
+    messagingSenderId: "1052191436339",
+    appId: "1:1052191436339:web:caf631defb338510847d46"
+};
+
+firebase.initializeApp(firebaseConfig);
